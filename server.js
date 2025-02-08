07c4141d6f226f7276f583c85c84587fdd323367
Kshirod Patel <kshirod.patel@gmail.com>
2025-02-08 23:47:53 +0530

    Updated code

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 import express from "express";
+import { PORT } from "./config/env.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routers.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
-import { PORT } from "./config/env.js";
-console.log("PORT", PORT);
 
+app.use(express.json());
+
+//Static route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//Auth Route
+app.use("/api/v1/auth", authRouter);
+
+//Users Route
+app.use("/api/v1/users", userRouter);
+
+//Subscriptions Route
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.listen(PORT, () => {
   console.log(`Subscription Tracker started on Port ${PORT}`);
